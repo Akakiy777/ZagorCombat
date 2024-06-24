521fe7d0ded6946a407a97044f5d13f11765888e
Akakiy777 <136377706+Akakiy777@users.noreply.github.com>
2024-06-24 19:44:40 +0300

    Update Normal3.js

--- Normal3.js
+++ Normal3.js
@@ -13,10 +13,10 @@ jQuery(document).ready(function(){
         jQuery('.nor2').remove();
         var lol1 = '<div class="nor2">Баланс: '
         var lol2 = '<br>Кейс 0$-50$</div>'
-        if (money>=30){
-            var lopy = Math.floor(Math.random() * (50 - 0) + 0);
+        if (money>=100){
+            var lopy = Math.floor(Math.random() * (200 - 50) + 50);
             var lopy2 = 'Кейс открыт +'
-            money = money-30+lopy
+            money = money-100+lopy
             alert(lopy2+lopy);
         }else{
             alert('Не хватает денег!');
