c02e2f964a686301f5ef1d60387fc631254ad6fe
Akakiy777 <136377706+Akakiy777@users.noreply.github.com>
2024-06-24 21:39:34 +0300

    Update Normal3.js

--- Normal3.js
+++ Normal3.js
@@ -2,15 +2,13 @@ jQuery(document).ready(function(){
     var money = 50
     var taber = 1
     jQuery(".image-button").on('click',function(){
-        jQuery('.nor2').remove();
         var lol1 = '<div class="nor2">Баланс: '
         var lol2 = '<br>Кейс 0$-200$</div>'
         money = money+taber
         var lol3 = lol1+money+lol2
-        jQuery(this).parent().append(lol3)
+        jQuery('.nor2').html(lol3);
     });
     jQuery(".nor3").on('click',function(){
-        jQuery('.nor2').remove();
         var lol1 = '<div class="nor2">Баланс: '
         var lol2 = '<br>Кейс 0$-200$</div>'
         if (money>=100){
@@ -22,58 +20,52 @@ jQuery(document).ready(function(){
             alert('Не хватает денег!');
         }
         var lol3 = lol1+money+lol2
-        jQuery('.image-button').parent().append(lol3)
+        jQuery('.nor2').html(lol3);
     });
     jQuery(".lo1").on('click',function(){
         if (money>=10){
-            jQuery('.nor2').remove();
             var lol1 = '<div class="nor2">Баланс: '
             var lol2 = '<br>Кейс 0$-200$</div>'
             money = money-10
             var lol3 = lol1+money+lol2
-            jQuery('.image-button').parent().append(lol3)
+            jQuery('.nor2').html(lol3);
             taber+=1
-            jQuery('.not0').remove();
             var kol1 = '<div class="not0"> >>> '
             var kol2 = ' <<< </div>'
             var pon4 = kol1+taber+kol2
-            jQuery('.not1').parent().append(pon4)
+            jQuery('.not0').html(pon4);
         }else{
             alert('Не хватает денег!');
         }
     });
     jQuery(".lo2").on('click',function(){
-        if (money>=20){
-            jQuery('.nor2').remove();
+        if (money>=10){
             var lol1 = '<div class="nor2">Баланс: '
             var lol2 = '<br>Кейс 0$-200$</div>'
-            money = money-20
+            money = money-10
             var lol3 = lol1+money+lol2
-            jQuery('.image-button').parent().append(lol3)
-            taber+=2
-            jQuery('.not0').remove();
+            jQuery('.nor2').html(lol3);
+            taber+=1
             var kol1 = '<div class="not0"> >>> '
             var kol2 = ' <<< </div>'
             var pon4 = kol1+taber+kol2
-            jQuery('.not1').parent().append(pon4)
+            jQuery('.not0').html(pon4);
         }else{
             alert('Не хватает денег!');
         }
     });
     jQuery(".lo3").on('click',function(){
-        if (money>=50){
-            jQuery('.nor2').remove();
+        if (money>=10){
             var lol1 = '<div class="nor2">Баланс: '
             var lol2 = '<br>Кейс 0$-200$</div>'
-            money = money-50
+            money = money-10
             var lol3 = lol1+money+lol2
-            jQuery('.image-button').parent().append(lol3)
-            taber+=5
-            jQuery('.not0').remove();
+            jQuery('.nor2').html(lol3);
+            taber+=1
             var kol1 = '<div class="not0"> >>> '
             var kol2 = ' <<< </div>'
             var pon4 = kol1+taber+kol2
-            jQuery('.not1').parent().append(pon4)
+            jQuery('.not0').html(pon4);
         }else{
             alert('Не хватает денег!');
         }
@@ -81,48 +73,43 @@ jQuery(document).ready(function(){
     jQuery('.reg0').on('click',function(){
         var promocod = document.getElementById('bu1').value
         if (promocod == "hamster"){
-            jQuery('.nor2').remove();
             var lol1 = '<div class="nor2">Баланс: '
             var lol2 = '<br>Кейс 0$-200$</div>'
             money = money+5000
             var lol3 = lol1+money+lol2
-            jQuery('.image-button').parent().append(lol3)
+            jQuery('.nor2').html(lol3);
             alert('Успешно');
         }
         if (promocod == "oleg"){
-            jQuery('.nor2').remove();
             var lol1 = '<div class="nor2">Баланс: '
             var lol2 = '<br>Кейс 0$-200$</div>'
             money = money+1000
             var lol3 = lol1+money+lol2
-            jQuery('.image-button').parent().append(lol3)
+            jQuery('.nor2').html(lol3);
             alert('Успешно');
         }
         if (promocod == "hui"){
-            jQuery('.nor2').remove();
             var lol1 = '<div class="nor2">Баланс: '
             var lol2 = '<br>Кейс 0$-200$</div>'
             money = money-5000
             var lol3 = lol1+money+lol2
-            jQuery('.image-button').parent().append(lol3)
+            jQuery('.nor2').html(lol3);
             alert('Успешно');
         }
     })
     jQuery('.re1').on('click',function(){
         var saves = document.getElementById('bu2').value.split(' ')
         if (saves.length == 2) {
-            jQuery('.nor2').remove();
             var lol1 = '<div class="nor2">Баланс: '
             var lol2 = '<br>Кейс 0$-200$</div>'
             money = Number(saves[0])
             var lol3 = lol1+money+lol2
-            jQuery('.image-button').parent().append(lol3)
-            jQuery('.not0').remove();
+            jQuery('.nor2').html(lol3);
             taber = Number(saves[1])
             var kol1 = '<div class="not0"> >>> '
             var kol2 = ' <<< </div>'
             var pon4 = kol1+Number(saves[1])+kol2
-            jQuery('.not1').parent().append(pon4)
+            jQuery('.not0').html(pon4);
             alert('Успешно');
         }
     })
